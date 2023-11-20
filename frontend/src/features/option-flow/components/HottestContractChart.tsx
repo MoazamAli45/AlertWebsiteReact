e869ce704ddd1e20c6d3902c2f146f53b32c185a
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2023-11-20 17:35:46 +0500

    Updated

--- frontend/src/features/option-flow/components/HottestContractChart.tsx
+++ frontend/src/features/option-flow/components/HottestContractChart.tsx
@@ -66,6 +66,7 @@ export const HottestPurcahseChart = () => {
       >
         Hottest Contract
       </Typography>
+
       <Box
         sx={{
           position: 'relative',
@@ -94,7 +95,7 @@ export const HottestPurcahseChart = () => {
           </TableHead>
           <TableBody>
             {isLoading &&
-              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+              [1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
                 <TableRow
                   key={i}
                   sx={{
